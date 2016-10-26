4f8850afc884d71b8d04db67323952d7b87e83cc
Sher Chowdhury <sherc1980@yahoo.com>
2016-10-26 16:41:17 +0100

    bf

--- scripts/update-git.sh
+++ scripts/update-git.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo '##########################################################################'
+echo '################ About to run update-git.sh script #######################'
+echo '##########################################################################'
 
+yum -y install wget || exit 1
 yum -y install curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils-MakeMaker
 
 cd /usr/src
-wget https://www.kernel.org/pub/software/scm/git/git-1.9.4.tar.gz
+wget https://www.kernel.org/pub/software/scm/git/git-1.9.4.tar.gz || exit 1
 tar xzf git-1.9.4.tar.gz
 
 cd git-1.9.4
