d85fc197b5e77fb814869100da9b2de2438a9d10
Sher Chowdhury <sherc1980@yahoo.com>
2016-10-26 22:35:52 +0100

    updated git version

--- scripts/update-git.sh
+++ scripts/update-git.sh
@@ -4,14 +4,17 @@ echo '##########################################################################
 echo '################ About to run update-git.sh script #######################'
 echo '##########################################################################'
 
+# http://tecadmin.net/install-git-2-x-on-centos-rhel-and-fedora/#
+
+
 yum -y install wget || exit 1
 yum -y install curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils-MakeMaker
 
 cd /usr/src
-wget https://www.kernel.org/pub/software/scm/git/git-1.9.4.tar.gz || exit 1
-tar xzf git-1.9.4.tar.gz
+wget https://www.kernel.org/pub/software/scm/git/git-2.8.1.tar.gz 
+tar xzf git-2.8.1.tar.gz
 
-cd git-1.9.4
+cd git-2.8.1
 make prefix=/usr/local/git all
 make prefix=/usr/local/git install
 echo "export PATH=/usr/local/git/bin:$PATH" >> /etc/bashrc
