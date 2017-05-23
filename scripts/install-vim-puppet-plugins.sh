95e7ca162bd628f30f9e82e737f15de0799cdf74
Sher Chowdhury <sher.chowdhury@gmail.com>
2017-05-23 11:54:13 +0100

    Added shellcheck, which is a bash script syntax checker

--- scripts/install-vim-puppet-plugins.sh
+++ scripts/install-vim-puppet-plugins.sh
@@ -5,6 +5,7 @@ echo '##########################################################################
 
 sudo yum install -y epel-release || exit 1
 sudo yum install -y vim || exit 1
+sudo yum install -y ShellCheck || exit 1
 
 # Some useful plugins: http://vimawesome.com/
 
